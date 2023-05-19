d686b3758935d4af06b0e45550e97768e1306a50
Jeff Fairley <jfairley@gmail.com>
2023-05-19 17:45:20 -0600

    add coverage log within test

--- cypress/e2e/button.cy.js
+++ cypress/e2e/button.cy.js
@@ -4,5 +4,8 @@ describe("Button", () => {
   it("should render", () => {
     cy.visit("http://localhost:6006/iframe.html?id=example-button--primary");
     cy.get(".storybook-button").should("have.text", "Button");
+    cy.window().then((win) => {
+      cy.log("coverage object", win.__coverage__);
+    });
   });
 });
